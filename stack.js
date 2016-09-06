770c556f6b34e906c144c7f434eb12f38bba1bc6
Bill O'Brien <airman.obrien@gmail.com>
2016-09-06 15:55:21 -0400

    changed array

--- stack.js
+++ stack.js
@@ -1,34 +1,32 @@
 var StackClass = {
 
+    x: [],
 
-    push : function(element) {
+    push: function(element) {
         this.x.push(element);
     },
 
-    pop : function() {
+    pop: function() {
         return this.x.pop();
     },
 
-    peek : function() {
-        return this.x[this.x.length-1];
+    peek: function() {
+        return this.x[this.x.length - 1];
     }
 
 
 }
 
-function StackClassConstructor(){
+function StackClassConstructor() {
     var o = Object.create(StackClass);
-    o.x= [];
+    
     return o;
 }
 var sc = StackClassConstructor();
- 
+
 sc.push(1);
 sc.push(2);
 sc.push(3);
 sc.push(4);
 sc.pop();
 console.log(sc.peek());
-
-
-
