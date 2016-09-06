34b9562942195d27bc7c1dff62d1dbe3c3cbad77
Bill O'Brien <airman.obrien@gmail.com>
2016-09-06 15:25:52 -0400

    Created StackClass.js

---
+++ stack.js
@@ -0,0 +1,34 @@
+var StackClass = {
+
+
+    push : function(element) {
+        this.x.push(element);
+    },
+
+    pop : function() {
+        return this.x.pop();
+    },
+
+    peek : function() {
+        return this.x[this.x.length-1];
+    }
+
+
+}
+
+function StackClassConstructor(){
+    var o = Object.create(StackClass);
+    o.x= [];
+    return o;
+}
+var sc = StackClassConstructor();
+ 
+sc.push(1);
+sc.push(2);
+sc.push(3);
+sc.push(4);
+sc.pop();
+console.log(sc.peek());
+
+
+
